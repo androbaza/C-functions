23afef7225201bdab3f0a1a951c394a5fea43782
Andrey YERSHOV <yershov_a@qwasar.io>
2020-05-27 16:41:12 +0000

    first try

---
+++ my_ls.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include "my_ls.h"
+
+int param_a()
+{
+    return 0;
+}
+
+int param_R()
+{
+    return 0;
+}
+
+int param_t()
+{
+    return 0;
+}
+
+int main()
+{
+    return 0;
+}
